47611e8b488ee2e52cb3246f8b75f8d01e0032d5
Darcy Matheson <darcy.matheson10@gmail.com>
2021-11-15 23:46:08 +1100

    Merged Beta
    Added... lots of things. Main scene has not been set up, but files are together and non-conflicted.

--- Assets/Will K/Scripts/TriggerDisplay.cs
+++ Assets/Will K/Scripts/TriggerDisplay.cs
@@ -6,17 +6,20 @@ public class TriggerDisplay : MonoBehaviour
 {
     void OnDrawGizmos()
     {
-        Vector3 drawBoxVector = new Vector3(
-            this.transform.lossyScale.x * this.GetComponent<BoxCollider>().size.x,
-            this.transform.lossyScale.y * this.GetComponent<BoxCollider>().size.y,
-            this.transform.lossyScale.z * this.GetComponent<BoxCollider>().size.z);
+        if (this.GetComponent<BoxCollider>() == true)
+        {
+            Vector3 drawBoxVector = new Vector3(
+                this.transform.lossyScale.x * this.GetComponent<BoxCollider>().size.x,
+                this.transform.lossyScale.y * this.GetComponent<BoxCollider>().size.y,
+                this.transform.lossyScale.z * this.GetComponent<BoxCollider>().size.z);
 
-        Vector3 drawBoxPosition = this.transform.position + this.GetComponent<BoxCollider>().center;
+            Vector3 drawBoxPosition = this.transform.position + this.GetComponent<BoxCollider>().center;
 
-        Gizmos.matrix = Matrix4x4.TRS(drawBoxPosition, this.transform.rotation, drawBoxVector);
-        Gizmos.color = new Color(1, 1, 1, 0.2f);
-        Gizmos.DrawCube(Vector3.zero, Vector3.one);
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
+            Gizmos.matrix = Matrix4x4.TRS(drawBoxPosition, this.transform.rotation, drawBoxVector);
+            Gizmos.color = new Color(1, 1, 1, 0.2f);
+            Gizmos.DrawCube(Vector3.zero, Vector3.one);
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
+        }
     }
 }
